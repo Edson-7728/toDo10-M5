e6c6630fa3495f573f833e026046c764e781de1d
Edson Vieira <elvieira.275@gmail.com>
2022-08-29 18:33:00 -0300

    alteracao no codigo

---
+++ src/pages/+Cadastrar/Cadastrar.jsx
@@ -0,0 +1,12 @@
+import S from './Cadastrar.module.css'
+import Formulario from '../../components/Itens/Formulario'
+
+const Cadastrar = () => {
+  return (
+    <div className={S.divContainer}>
+        <Formulario />        
+    </div>
+  )
+}
+
+export default Cadastrar
