6c80308ec4832c5d78b778540e583f3a574dfea1
Edson Vieira <elvieira.275@gmail.com>
2022-08-29 02:28:33 -0300

    feat: criada comp header e sua estilizacao

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import S from './Header.module.css'
+import { Link } from 'react-router-dom'
+
 
 const Header = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div className={S.divContainer}>
+            <header className={S.header}>
+                <h1>Venture Labs</h1>
+                <h2>Questionário</h2>
+                <nav>
+                <Link className={S.link} to='/cadastrar'>+ Cadastrar</Link>
+                <Link className={S.link} to='/clientes'>Clientes</Link>
+                </nav>
+            </header>
+        </div>
+    )
 }
 
 export default Header
