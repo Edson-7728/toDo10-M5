1bd8d19027de9afc80e42e4df6799a32af721239
Edson Vieira <elvieira.275@gmail.com>
2022-08-30 01:52:03 -0300

    alteracao no codigo

--- src/pages/Clientes/Clientes.jsx
+++ src/pages/Clientes/Clientes.jsx
@@ -1,7 +1,11 @@
+import React from 'react'
 import S from './Clientes.module.css'
+import {CadastroContext} from '../../context/Cadastro'
+import { useContext } from 'react'
 import Card1 from '../../components/cards/Card1'
 
 const Clientes = () => {
+    const {campo} = useContext(CadastroContext);
     return (
         <div className={S.divCampo}>
             {campo.length > 0 ? campo.map((item) => <Card1 nome={item.nome} sobrenome={item.sobrenome} telefone={item.telefone} cep={item.cep} endereco1={item.endereco1} endereco2={item.endereco2} dataNascimento={item.dataNascimento} cpf={item.cpf} rendaMensal={item.rendaMensal} />)
