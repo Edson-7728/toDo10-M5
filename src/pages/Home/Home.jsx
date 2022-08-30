15c0821fbb354995c56832e3078c056d4194f7fc
Edson Vieira <elvieira.275@gmail.com>
2022-08-30 01:49:43 -0300

    ajuste no codigo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
-import Header from '../../components/Header/Header'
+import React from 'react'
 
 const Home = () => {
     return (
         <div>
-            <Header />
+            <h1>Bem vindos!</h1>
+            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita non quam mollitia saepe accusantium atque maiores dolores hic, nostrum incidunt eligendi in sequi vero minima! Asperiores iste mollitia praesentium iusto!</h2>
         </div>
     )
 }
